89754852f50fcb44df012f5c1041253d8599a462
Mike EVMM <mikemakesgames@gmail.com>
2016-04-23 00:43:41 +0100

    First commit.

---
+++ source/states/MainState.hx
@@ -0,0 +1,43 @@
+package states;
+import entities.SortableSpriteGroup;
+import entities.character.CollisionMap;
+import entities.character.Harpae;
+import flixel.FlxCamera.FlxCameraFollowStyle;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import openfl.display.BlendMode;
+
+/**
+ * ...
+ * @author Miguel M.
+ */
+class MainState extends SortState
+{
+	public function new()
+	{
+		super();
+	}
+	
+	private var harp:Harpae;
+	
+	override public function create():Void 
+	{
+		super.create();
+		
+		FlxG.camera.bgColor = FlxColor.WHITE;
+		
+		addSortedSprite(new FlxSprite(0, 0, "assets/images/test/layer1.png"), 1).scrollFactor.set(0.8, 0.8);
+		
+		var layer2:FlxSprite = new FlxSprite(0, 0, "assets/images/test/layer2multiply.png");
+		layer2.blend = BlendMode.MULTIPLY;
+		addSortedSprite(layer2, 2);
+		
+		var layer3:FlxSprite = new FlxSprite(0, 0, "assets/images/test/layer3add.png");
+		layer3.blend = BlendMode.ADD;
+		addSortedSprite(layer3, 3);
+		
+		addSortedSprite(new FlxSprite(0, 0, "assets/images/test/parallax.png"), -1);
+	}
+}
